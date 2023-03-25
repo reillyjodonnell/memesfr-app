8e9248c6f0eb307637b5f07808a610cd6e3626ec
Reilly O'Donnell <odonnell.reilly.j@gmail.com>
2023-03-25 00:28:45 -0400

    continued implementing the profile ui

--- src/profile/profile.tsx
+++ src/profile/profile.tsx
@@ -1,4 +1,4 @@
-import {Text, View} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
 import UserAvatar from '../components/user-avatar';
 import {colors} from '../theme';
 import React from 'react';
@@ -8,6 +8,7 @@ export default function Profile({
   memes = 0,
   followers = 0,
   following = 0,
+  isFollowing = true,
 }) {
   return (
     <View
@@ -24,7 +25,9 @@ export default function Profile({
           height: '20%',
           backgroundColor: colors.accent,
           width: '100%',
+          borderRadius: colors.borderRadius.rounded,
         }}></View>
+
       <View style={{width: colors.avatar.xl, height: colors.avatar.xl}}>
         <UserAvatar />
       </View>
@@ -44,90 +47,156 @@ export default function Profile({
         </Text>
       </View>
 
+      <ProfileStats memes={memes} followers={followers} following={following} />
       <View
         style={{
-          display: 'flex',
           flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-          position: 'relative',
+          width: '100%',
+          paddingVertical: colors.spacing.l,
+          justifyContent: 'space-evenly',
         }}>
-        <View
+        <Pressable
           style={{
-            flexDirection: 'row',
-            width: '100%',
+            borderRadius: colors.borderRadius.rounded,
+            borderWidth: colors.border.regular,
+            borderColor: colors.accent,
+            backgroundColor: isFollowing
+              ? colors.accentHighlight
+              : colors.accent,
+            paddingVertical: colors.spacing.s,
+            paddingHorizontal: colors.spacing.m,
+            width: '40%',
             justifyContent: 'center',
             alignItems: 'center',
           }}>
-          <View
+          <Text
             style={{
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
+              color: colors.textPrimary,
+              fontSize: colors.fontXL,
+              fontWeight: colors.fontBold,
             }}>
-            <Text
-              style={{
-                color: colors.textPrimary,
-                fontWeight: colors.fontBold,
-                fontSize: colors.fontLg,
-              }}>
-              {memes}
-            </Text>
-            <Text
-              style={{
-                color: colors.textSecondary,
-                fontSize: colors.fontMd,
-                fontWeight: colors.fontBold,
-              }}>
-              Memes
-            </Text>
-          </View>
-          <View
+            {isFollowing ? 'Following' : 'Follow'}
+          </Text>
+        </Pressable>
+        <Pressable
+          style={{
+            borderRadius: colors.borderRadius.rounded,
+            borderColor: colors.textSecondary,
+            borderWidth: colors.border.regular,
+            paddingVertical: colors.spacing.s,
+            paddingHorizontal: colors.spacing.m,
+            width: '40%',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text
             style={{
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
+              color: colors.textPrimary,
+              fontSize: colors.fontXL,
+              fontWeight: colors.fontBold,
             }}>
-            <Text
-              style={{
-                color: colors.textPrimary,
-                fontWeight: colors.fontBold,
-                fontSize: colors.fontLg,
-              }}>
-              {following}
-            </Text>
-            <Text
-              style={{
-                color: colors.textSecondary,
-                fontSize: colors.fontMd,
-                fontWeight: colors.fontBold,
-              }}>
-              Following
-            </Text>
-          </View>
-          <View
+            Message
+          </Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+function ProfileStats({memes = 0, followers = 0, following = 0}) {
+  return (
+    <View
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+      }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingHorizontal: colors.spacing.m,
+        }}>
+        <View
+          style={{
+            flexDirection: 'column',
+            borderRightColor: colors.line,
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '33%',
+          }}>
+          <Text
             style={{
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
+              color: colors.textPrimary,
+              fontWeight: colors.fontBold,
+              fontSize: colors.fontLg,
             }}>
-            <Text
-              style={{
-                color: colors.textPrimary,
-                fontWeight: colors.fontBold,
-                fontSize: colors.fontLg,
-              }}>
-              {followers}
-            </Text>
-            <Text
-              style={{
-                color: colors.textSecondary,
-                fontSize: colors.fontMd,
-                fontWeight: colors.fontBold,
-              }}>
-              Followers
-            </Text>
-          </View>
+            {memes}
+          </Text>
+          <Text
+            style={{
+              color: colors.textSecondary,
+              fontSize: colors.fontMd,
+              fontWeight: colors.fontBold,
+            }}>
+            Memes
+          </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '33%',
+            borderLeftColor: colors.line,
+            borderRightColor: colors.line,
+            borderLeftWidth: colors.border.regular,
+            borderRightWidth: colors.border.regular,
+          }}>
+          <Text
+            style={{
+              color: colors.textPrimary,
+              fontWeight: colors.fontBold,
+              fontSize: colors.fontLg,
+            }}>
+            {following}
+          </Text>
+          <Text
+            style={{
+              color: colors.textSecondary,
+              fontSize: colors.fontMd,
+              fontWeight: colors.fontBold,
+            }}>
+            Following
+          </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '33%',
+          }}>
+          <Text
+            style={{
+              color: colors.textPrimary,
+              fontWeight: colors.fontBold,
+              fontSize: colors.fontLg,
+            }}>
+            {followers}
+          </Text>
+          <Text
+            style={{
+              color: colors.textSecondary,
+              fontSize: colors.fontMd,
+              fontWeight: colors.fontBold,
+            }}>
+            Followers
+          </Text>
         </View>
       </View>
     </View>
